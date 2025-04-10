2dc3ef1492650511fab8f15ae1bd73b001c2af80
Divyabit <divya.it22@bitsathy.ac.in>
2025-04-10 16:32:11 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-      <footer className="bg-white text-center py-6 shadow-inner">
-        <p className="text-gray-600">© {new Date().getFullYear()} Divya K. All Rights Reserved.</p>
+      <footer className="bg-black text-center py-6 shadow-inner">
+        <p className="text-white-600">© {new Date().getFullYear()} Divya K. All Rights Reserved.</p>
         <p className="text-sm text-purple-500 mt-1">Made with 💜 using React & Tailwind CSS</p>
       </footer>
     );
